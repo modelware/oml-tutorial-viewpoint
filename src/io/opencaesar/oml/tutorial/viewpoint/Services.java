c016f67d6ad79b4f1371d80f6c7b084aa0bd7585
Maged Elaasar <melaasar@gmail.com>
2021-03-21 16:34:05 -0700

    More features in entity diagrams

--- src/io/opencaesar/oml/tutorial/viewpoint/Services.java
+++ src/io/opencaesar/oml/tutorial/viewpoint/Services.java
@@ -1,12 +1,30 @@
 package io.opencaesar.oml.tutorial.viewpoint;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.eclipse.xtext.resource.XtextResource;
 
 import io.opencaesar.oml.AnnotatedElement;
 import io.opencaesar.oml.AnnotationProperty;
+import io.opencaesar.oml.Entity;
+import io.opencaesar.oml.EntityReference;
 import io.opencaesar.oml.ForwardRelation;
 import io.opencaesar.oml.LinkAssertion;
 import io.opencaesar.oml.Literal;
@@ -16,11 +34,16 @@ import io.opencaesar.oml.OmlFactory;
 import io.opencaesar.oml.PropertyValueAssertion;
 import io.opencaesar.oml.Reference;
 import io.opencaesar.oml.Relation;
+import io.opencaesar.oml.RelationCardinalityRestrictionAxiom;
 import io.opencaesar.oml.RelationEntity;
 import io.opencaesar.oml.RelationInstance;
+import io.opencaesar.oml.RelationRangeRestrictionAxiom;
+import io.opencaesar.oml.RelationRestrictionAxiom;
+import io.opencaesar.oml.RelationTargetRestrictionAxiom;
 import io.opencaesar.oml.ReverseRelation;
 import io.opencaesar.oml.ScalarProperty;
 import io.opencaesar.oml.ScalarPropertyValueAssertion;
+import io.opencaesar.oml.Vocabulary;
 import io.opencaesar.oml.util.OmlFactory2;
 import io.opencaesar.oml.util.OmlRead;
 import io.opencaesar.oml.util.OmlSearch;
@@ -155,6 +178,133 @@ public class Services {
 		} else {
 			entity.getReverseRelation().setName(name);
 		}
+	}
+
+	public static Set<Entity> getVisualizedEntities(Vocabulary vocabulary) {
+		var entities = new HashSet<Entity>();
+		// direct entities
+		entities.addAll(vocabulary.getOwnedStatements().stream().
+				filter(s -> s instanceof Entity).
+				map(s -> (Entity)s).
+				collect(Collectors.toSet()));
+		// reference entities
+		entities.addAll(vocabulary.getOwnedStatements().stream().
+				filter(s -> s instanceof EntityReference).
+				map(s -> (Entity) OmlRead.resolve((EntityReference)s)).
+				collect(Collectors.toSet()));
+		// specialized entities
+		entities.addAll(vocabulary.getOwnedStatements().stream().
+				filter(s -> s instanceof Entity).
+				map(s -> (Entity)s).
+				flatMap(e -> e.getOwnedSpecializations().stream()).
+				map(s -> (Entity) s.getSpecializedTerm()).
+				collect(Collectors.toSet()));
+		entities.addAll(vocabulary.getOwnedStatements().stream().
+				filter(s -> s instanceof EntityReference).
+				map(s -> (EntityReference)s).
+				flatMap(e -> e.getOwnedSpecializations().stream()).
+				map(s -> (Entity) s.getSpecializedTerm()).
+				collect(Collectors.toSet()));
+		// related entities
+		entities.addAll(vocabulary.getOwnedStatements().stream().
+				filter(e -> e instanceof RelationEntity).
+				map(e -> (RelationEntity)e).
+				flatMap(r -> Stream.of(r.getSource(), r.getTarget())).
+				collect(Collectors.toSet()));
+		// range restricted entities
+		entities.addAll(vocabulary.getOwnedStatements().stream().
+				filter(s -> s instanceof Entity).
+				map(s -> (Entity)s).
+				flatMap(e -> e.getOwnedRelationRestrictions().stream()).
+				filter(r -> r instanceof RelationRangeRestrictionAxiom).
+				map(r -> ((RelationRangeRestrictionAxiom)r).getRange()).
+				collect(Collectors.toSet()));
+		entities.addAll(vocabulary.getOwnedStatements().stream().
+				filter(s -> s instanceof EntityReference).
+				map(s -> (EntityReference)s).
+				flatMap(e -> e.getOwnedRelationRestrictions().stream()).
+				filter(r -> r instanceof RelationRangeRestrictionAxiom).
+				map(r -> ((RelationRangeRestrictionAxiom)r).getRange()).
+				collect(Collectors.toSet()));
+		// cardinality range restricted entities
+		entities.addAll(vocabulary.getOwnedStatements().stream().
+				filter(s -> s instanceof Entity).
+				map(s -> (Entity)s).
+				flatMap(e -> e.getOwnedRelationRestrictions().stream()).
+				filter(r -> r instanceof RelationCardinalityRestrictionAxiom).
+				map(r -> ((RelationCardinalityRestrictionAxiom)r).getRange()).
+				collect(Collectors.toSet()));
+		entities.addAll(vocabulary.getOwnedStatements().stream().
+				filter(s -> s instanceof EntityReference).
+				map(s -> (EntityReference)s).
+				flatMap(e -> e.getOwnedRelationRestrictions().stream()).
+				filter(r -> r instanceof RelationCardinalityRestrictionAxiom).
+				map(r -> ((RelationCardinalityRestrictionAxiom)r).getRange()).
+				collect(Collectors.toSet()));
+		return entities;
+	}
+
+	public static Set<NamedInstance> getVisualizedNamedInstances(Vocabulary vocabulary) {
+		var instances = new HashSet<NamedInstance>();
+		// target restricted instances
+		instances.addAll(vocabulary.getOwnedStatements().stream().
+				filter(s -> s instanceof Entity).
+				map(s -> (Entity)s).
+				flatMap(e -> e.getOwnedRelationRestrictions().stream()).
+				filter(r -> r instanceof RelationTargetRestrictionAxiom).
+				map(r -> ((RelationTargetRestrictionAxiom)r).getTarget()).
+				collect(Collectors.toSet()));
+		instances.addAll(vocabulary.getOwnedStatements().stream().
+				filter(s -> s instanceof EntityReference).
+				map(s -> (EntityReference)s).
+				flatMap(e -> e.getOwnedRelationRestrictions().stream()).
+				filter(r -> r instanceof RelationTargetRestrictionAxiom).
+				map(r -> ((RelationTargetRestrictionAxiom)r).getTarget()).
+				collect(Collectors.toSet()));
+		return instances;
+	}
+	
+	public static Set<RelationRestrictionAxiom> getVisualizedRestrictions(Vocabulary vocabulary) {
+		var restrictions = new HashSet<RelationRestrictionAxiom>();
+		restrictions.addAll(vocabulary.getOwnedStatements().stream().
+				filter(s -> s instanceof Entity).
+				map(s -> (Entity)s).
+				flatMap(e -> e.getOwnedRelationRestrictions().stream()).
+				collect(Collectors.toSet()));
+		restrictions.addAll(vocabulary.getOwnedStatements().stream().
+				filter(s -> s instanceof EntityReference).
+				map(s -> (EntityReference) s).
+				flatMap(e -> e.getOwnedRelationRestrictions().stream()).
+				collect(Collectors.toSet()));
+		return restrictions;
+	}
+
+	public EObject openTextEditor(EObject any) {
+		if (any != null && any.eResource() instanceof XtextResource && any.eResource().getURI() != null) {
+
+			String fileURI = any.eResource().getURI().toPlatformString(true);
+			IFile workspaceFile = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(fileURI));
+			if (workspaceFile != null) {
+				IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				try {
+					IEditorPart openEditor = IDE.openEditor(page, workspaceFile, "io.opencaesar.oml.dsl.Oml", true);
+					if (openEditor instanceof AbstractTextEditor) {
+						ICompositeNode node = NodeModelUtils.findActualNodeFor(any);
+						if (node != null) {
+							int offset = node.getOffset();
+							int length = node.getTotalEndOffset() - offset;
+							((AbstractTextEditor) openEditor).selectAndReveal(offset, length);
+							System.out.println("Services.openTextEditor()");
+						}
+					}
+					// editorInput.
+				} catch (PartInitException e) {
+					// Put your exception handler here if you wish to.
+				}
+			}
+		}
+		System.out.println(any);
+		return any;
 	}
 
 }
